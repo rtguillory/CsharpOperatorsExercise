d90b100f83fb6369f20745cc123957ea1136943a
Rhonda Guillory <guilloryrt.ps@gmail.com>
2022-02-04 23:05:15 -0600

    initial commit

---
+++ CsharpOperatorsExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Exercise 1 -- C# Operators
+//declare and initialize variables
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+
+if(a == 17 && b == 4) 
+{
+    Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+}
